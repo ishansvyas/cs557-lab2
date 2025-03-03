a11b0a688a70d81a645308ae154e610eaf2c8089
Ishan Vyas <ishan.vyas@duke.edu>
2025-03-02 20:19:37 -0500

    sha3Wrapper changes

--- src/main/scala/sha3Wrapper.scala
+++ src/main/scala/sha3Wrapper.scala
@@ -15,33 +15,64 @@ class sha3Wrapper(W: Int)(implicit p: Parameters) extends AcceleratorCore {
   val bytes_per_word = W / 8
 
   val io = BeethovenIO(new AccelCommand("sha3") {
-    // input
-    val message = Flipped(new DecoupledIO(Vec(round_size_words, Bits(W.W))))
-
-    // output?
-    val hash = new DecoupledIO(Vec(hash_size_words, Bits(W.W)))
+    // i/o
+    val msg_addr = Address()
+    val hash_addr = Address()
   },
     EmptyAccelResponse())
 
+  // active command logic pt. 1
   val activeCmd = RegInit(false.B)
   when (io.req.fire) {
     activeCmd := true.B
   }
-
 
   // pairs with the AcceleratorSystemConfig in sha3WrapperConfig
   // create and initialize Reader and Writer modules
   val vec_in = getReaderModule("vec_in")
   val vec_out = getWriterModule("vec_out")
   vec_in.dataChannel.data.ready := false.B
+
+  /*
+  Notes: 1a. dataChannel comes from FPGA to Host
+         1b. requestChannel goes from Host to FPGA
+         if 1b is true; is vec_out initialized correctly?
+   */
+
+    // input channel initialization
   vec_in.requestChannel.valid := io.req.valid
-  vec_in.requestChannel.bits := io.req.bits
-//  vec_in.requestChannel.bits := ?????????????
+  vec_in.requestChannel.bits.addr := io.req.bits.msg_addr
+  vec_in.requestChannel.bits.len := c.U
+  vec_in.dataChannel.data.ready := false.B
 
+    // output channel initialization
+  vec_out.requestChannel.valid := io.req.valid
+  vec_out.requestChannel.bits.addr := io.req.bits.hash_addr
+  vec_out.requestChannel.bits.len := (hash_size_words * W).U// hash always 256
+  vec_out.dataChannel.data.bits := DontCare
+  vec_out.dataChannel.data.valid := false.B
 
+  /* --------------------------------
+  * HERE WE ADD THE SHA3 MODULE W/ IO (below line is applicable - don't ignore)
+  * ------------------------------- */
 
+//  vec_out.dataChannel.data.bits := // actual data
+  vec_out.dataChannel.data.valid := vec_in.dataChannel.data.valid && activeCmd
+
+  vec_in.dataChannel.data.ready := vec_out.dataChannel.data.ready
+
+  // actual output data
+  vec_out.dataChannel.data <> // some operation?
+
+  val all_channels_are_idle = vec_in.requestChannel.ready && vec_out.requestChannel.ready
+  io.req.ready := !activeCmd && all_channels_are_idle
+  io.resp.valid := activeCmd && all_channels_are_idle
+
+  // active command logic pt. 2
+
+  // IGNORE FOR NOW
   // instantiation definitely not correct
-  val sha3mod = Module(new Sha3Accel(W)(/*how to put DeqIO in here*/))
+//  val sha3mod = Module(new Sha3Accel(W)(/*how to put DeqIO in here*/))
 
 
 }
