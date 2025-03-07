63c979348c2eb1d604a13e2000092fa2b4eed81b
Ishan Vyas <ishan.vyas@duke.edu>
2025-03-06 22:12:55 -0500

    changes to sha3Wrapper draft1

--- src/main/scala/sha3Wrapper.scala
+++ src/main/scala/sha3Wrapper.scala
@@ -17,48 +17,54 @@ class sha3Wrapper(W: Int)(implicit p: Parameters) extends AcceleratorCore {
   val io = BeethovenIO(new AccelCommand("sha3") {
     // i/o
     val msg_addr = Address()
-    val hash_addr = Address()
-  },
-    EmptyAccelResponse())
+    val hash_addr = Address() // returns an address
 
+  }, EmptyAccelResponse())
   // active command logic pt. 1
   val activeCmd = RegInit(false.B)
   when (io.req.fire) {
     activeCmd := true.B
   }
-
   // pairs with the AcceleratorSystemConfig in sha3WrapperConfig
   // create and initialize Reader and Writer modules
   val vec_in = getReaderModule("vec_in")
   val vec_out = getWriterModule("vec_out")
   vec_in.dataChannel.data.ready := false.B
 
-    // input channel initialization
+    // vec_in initialization
+//  vec_in.requestChannel.ready    GIVEN
   vec_in.requestChannel.valid := io.req.valid
   vec_in.requestChannel.bits.addr := io.req.bits.msg_addr
-  vec_in.requestChannel.bits.len := c.U
-  vec_in.dataChannel.data.ready := false.B
+  vec_in.requestChannel.bits.len := common.Misc.multByIntPow2(round_size_words.U, 8) // 8 bc bigInt
+  vec_in.dataChannel.data.ready := true.B // in VectorAdd, never touched.
+//  vec_in.dataChannel.data.valid  GIVEN
+//  vec_in.dataChannel.data.bits   GIVEN
 
-    // output channel initialization
-  vec_out.requestChannel.valid := io.req.valid
-  vec_out.requestChannel.bits.addr := io.req.bits.hash_addr
-  vec_out.requestChannel.bits.len := (hash_size_words * W).U// hash always 256
+    // vec_out initialization ---------
+//  vec_out.requestChannel.ready   GIVEN
+  vec_out.requestChannel.valid := io.resp.valid
+  vec_out.requestChannel.bits.addr <> io.req.bits.hash_addr   // i think <> works but if not changed to :=
+  vec_out.requestChannel.bits.len := 256.U
+//  vec_out.dataChannel.data.ready  GIVEN
   vec_out.dataChannel.data.bits := DontCare
-  vec_out.dataChannel.data.valid := false.B
+  vec_out.dataChannel.data.valid := true.B // in VectorAdd, never touched
 
-  /* --------------------------------
-  * HERE WE ADD THE SHA3 MODULE W/ IO
-  * ------------------------------- */
+    // io initialization
+  io.req.ready := vec_in.requestChannel.ready && vec_out.requestChannel.ready && !activeCmd
+//  io.req.valid  GIVEN
+//  io.req.bits.msg_addr GIVEN
+//  io.resp.ready GIVEN
+  io.resp.valid := vec_in.requestChannel.ready && vec_out.requestChannel.ready && activeCmd
 
+  // BELOW --------------------------------------------
+  /*
+    // sha3_module initialization
   vec_out.dataChannel.data.valid := vec_in.dataChannel.data.valid && activeCmd
   vec_in.dataChannel.data.ready := vec_out.dataChannel.data.ready
 
   // actual output data
   val sha3_module = Module(new Sha3Accel(W))
 
-  sha3_module.io.message.bits := VecInit(
-    (0 until round_size_words).map(i => vec_in.dataChannel.data.bits( (i+1)*W - 1, i*W ))
-  )
   sha3_module.io.message.valid := vec_in.dataChannel.data.valid
   vec_in.dataChannel.data.ready := sha3_module.io.message.ready
 
@@ -70,30 +76,46 @@ class sha3Wrapper(W: Int)(implicit p: Parameters) extends AcceleratorCore {
   val all_channels_are_idle = vec_in.requestChannel.ready && vec_out.requestChannel.ready
   io.req.ready := !activeCmd && all_channels_are_idle
   io.resp.valid := activeCmd && all_channels_are_idle
-
+*/
+  // ABOVE -----------------------------------------
   // active command logic pt. 2
   when(io.resp.fire) {
     activeCmd := false.B
   }
 
-  // to do: state machine to hold the memory input
+  // to do: state machine to hold the memory input.
+    // assume sha3wrapper in core and NOT in rest.of.world.
+
   // reg file that holds 17 * 8 bytes.
   val inputRegFile = RegInit(VecInit(Seq.fill(17)(0.U(64.W))))
   val outputRegFile = RegInit(VecInit(Seq.fill(4)(0.U(64.W))))
-  val counter = RegInit(0.U(5.W))
+  val counter_in = RegInit(0.U(5.W))
+  val counter_out = RegInit(0.U(5.W))
+  val start_export = RegInit(false.B)
 
   // when valid/ready handshake, initiate counter to 17.
-  when (vec_in.dataChannel.data.valid && counter < 17.U) {
-      inputRegFile(counter) := vec_in.dataChannel.data.bits
-      counter := counter + 1.U
+  when (vec_in.dataChannel.data.valid && counter_in < 17.U) {
+      inputRegFile(counter_in) := vec_in.dataChannel.data.bits
+      counter_in := counter_in + 1.U
   }
   // then, once data stored, pass to sha3.input
-  when (counter === 17.U) {
-      sha3_module.io.hash.bits := inputRegFile
-      sha3_module.io.hash.valid := true.B
-      counter := 0.U
+  when (counter_in === 17.U) {
+    sha3_module.io.message.bits := inputRegFile
+    sha3_module.io.message.valid := true.B
+    counter_in := 0.U
   }
 
-  // to do: state machine to hold the memory output
   // same idea for sha3 output
+  // to do: state machine to hold the memory output
+  when (sha3_module.io.hash.valid && vec_out.dataChannel.data.ready) {
+    outputRegFile := sha3_module.io.hash.bits
+    start_export := true.B
+  }
+  when (start_export && counter_out < 4.U) {
+    vec_out.dataChannel.data.bits := outputRegFile(counter_out)
+    vec_out.dataChannel.data.valid := true.B
+    counter_out := counter_out + 1.U
+  }
+
+
 }
