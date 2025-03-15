91ccc35446cde2550b10b9a54fcd529b5d55094f
Ishan Vyas <ishan.vyas@duke.edu>
2025-03-15 11:25:48 -0400

    sha3 wrapper

--- src/main/c/sha3WrapperTest.cc
+++ src/main/c/sha3WrapperTest.cc
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <cstdint>
+#include <iostream>
 #include <beethoven/fpga_handle.h>
 #include <beethoven_hardware.h>
 
@@ -32,9 +33,11 @@ void dma_workaround_copy_from_fpga(remote_ptr &q) {
 int main() {
     // connection to the FPGA management runtime, needed in the TB.
     fpga_handle_t handle;
-
+    std::cout << "ran: fpga_handle_t handle;"
     // create array of 0s
     remote_ptr in_alloc = handle.malloc(sizeof(uint64_t) * 24);
+    std::cout << "ran: remote_ptr in_alloc = handle.malloc(sizeof(uint64_t) * 24);"
+
     uint64_t* host_alloc = (uint64_t*)in_alloc.getHostAddr();
     std::fill(host_alloc, host_alloc + 24, 0);
 
